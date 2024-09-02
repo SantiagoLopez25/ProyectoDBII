df9978a5eaa7d642986e3d823715bd38fc45e209
DSamuelQ <samuelquiji@gmail.com>
2024-09-02 12:00:31 -0600

    Commit Update Scripts in the repo

--- ProyectoBaseDatosII/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ ProyectoBaseDatosII/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d3591fb71c1fb88a60cf2a92008edbc07d453b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23809156b27ec5b109cc569b969e8570cfb13315")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
