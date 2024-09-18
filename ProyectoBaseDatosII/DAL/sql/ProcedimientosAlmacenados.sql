57364a9fc3a63914c1490b4bea7e8e63b2d80740
DSamuelQ <165442834+DSamuelQ@users.noreply.github.com>
2024-09-17 21:40:05 -0600

    Update ProcedimientosAlmacenados.sql

--- ProyectoBaseDatosII/DAL/sql/ProcedimientosAlmacenados.sql
+++ ProyectoBaseDatosII/DAL/sql/ProcedimientosAlmacenados.sql
@@ -6,7 +6,9 @@ GO
 -- Procedimiento para listar tipos de Proveedores
 CREATE PROC ListarProveedor
 AS
-SELECT * FROM Proveedor ORDER BY id_Proveedor ASC
+SELECT * FROM Proveedor
+WHERE Estado = 1
+ORDER BY id_Proveedor ASC
 GO
 
 --PRocedimiento para listar marcas
@@ -20,13 +22,17 @@ GO
 -- Procedimiento para listar Categor�as
 CREATE PROC ListarCategoria
 AS
-SELECT * FROM Categoria ORDER BY id_Categoria ASC
+SELECT * FROM Categoria 
+WHERE Estado = 1
+ORDER BY id_Categoria ASC
 GO
 
 -- Procedimiento para listar Empleado
 CREATE PROC ListarEmpleado
 AS
-SELECT * FROM Empleado ORDER BY id_Empleado ASC
+SELECT * FROM Empleado 
+WHERE Estado = 1
+ORDER BY id_Empleado ASC
 GO
 
 -- Procedimiento para registrar nuevo Proveedor
@@ -113,7 +119,7 @@ Create PROC ActualizarCategoria
 AS BEGIN
 	UPDATE Categoria SET
 	categoria=@Categoria,
-	Estado=@Estado,
+	Estado=@Estado
 	WHERE id_Categoria = @id_Categoria
 END;
 GO
@@ -146,7 +152,39 @@ AS BEGIN
     SET Estado = 0
     WHERE id_Marca = @id_Marca
 END;
+GO
 
+--Procedimiento para eliminar proveedores
+CREATE PROC EliminarProveedor
+@id_Proveedor int
+AS BEGIN
+    -- Cambiar el estado a 0 (falso) para ocultar el proveedor
+    UPDATE Proveedor 
+    SET Estado = 0
+    WHERE id_Proveedor = @id_Proveedor
+END;
+GO
+
+--Procedimiento para eliminar categoria
+CREATE PROC EliminarCategoria
+@id_Categoria int
+AS BEGIN
+    -- Cambiar el estado a 0 (falso) para ocultar la marca
+    UPDATE Categoria 
+    SET Estado = 0
+    WHERE id_Categoria = @id_Categoria
+END;
+GO
+
+--Procedimiento para eliminar empleado
+CREATE PROC EliminarEmpleado
+@id_Empleado int
+AS BEGIN
+    -- Cambiar el estado a 0 (falso) para ocultar la marca
+    UPDATE Empleado 
+    SET Estado = 0
+    WHERE id_Empleado = @id_Empleado
+END;
 GO
 
 -- Procedimiento almacenado que permite crear un usuario, actualizarlo y deshabilitarlo
@@ -184,7 +222,8 @@ BEGIN
 		end
 
 		select *from Usuario
-END
+END;
+GO
 
 
 --Procedimiento para listar usuarios
